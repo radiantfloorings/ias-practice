d5dcda3079d4a517a4c4bb05243aba48ef90fd75
radiantfloorings <rituagar1985@gmail.com>
2026-07-12 21:31:53 +0530

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,36 @@
+const questions=[
+
+{
+question:"Who is known as the Father of Indian Constitution?",
+options:[
+"Mahatma Gandhi",
+"Dr. B.R. Ambedkar",
+"Sardar Patel",
+"Jawaharlal Nehru"
+],
+answer:1
+},
+
+{
+question:"Capital of India?",
+options:[
+"Mumbai",
+"Delhi",
+"Kolkata",
+"Chennai"
+],
+answer:1
+},
+
+{
+question:"How many Fundamental Rights are there?",
+options:[
+"4",
+"5",
+"6",
+"7"
+],
+answer:2
+}
+
+];
