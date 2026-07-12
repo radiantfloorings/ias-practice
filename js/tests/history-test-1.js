3d0561fd594098b0d2b22a4493960457741b84a6
radiantfloorings <rituagar1985@gmail.com>
2026-07-12 22:12:55 +0530

    Create history-test-1.js

---
+++ js/tests/history-test-1.js
@@ -0,0 +1,39 @@
+const questions = [
+
+{
+question:"Who is known as the Father of Indian Constitution?",
+options:[
+"Mahatma Gandhi",
+"Dr. B.R. Ambedkar",
+"Sardar Patel",
+"Jawaharlal Nehru"
+],
+answer:1,
+explanation:"Dr. B.R. Ambedkar chaired the Drafting Committee."
+},
+
+{
+question:"Capital of India?",
+options:[
+"Mumbai",
+"Delhi",
+"Kolkata",
+"Chennai"
+],
+answer:1,
+explanation:"Delhi is the capital of India."
+},
+
+{
+question:"Who is called the Iron Man of India?",
+options:[
+"Gandhi",
+"Patel",
+"Nehru",
+"Subhash Bose"
+],
+answer:1,
+explanation:"Sardar Vallabhbhai Patel is known as the Iron Man of India."
+}
+
+];
